71faf4ecc51a8ca682faf9d6d53ab100567645be
AlexDev72 <Alexelmtk72000@gmail.com>
2025-05-05 13:37:15 +0200

    Premier commit

---
+++ src/main/java/com/prelev/apirest_springboot/ApirestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.prelev.apirest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestSpringbootApplication.class, args);
+	}
+
+}
